1fb310f5b798d46ffd6e55373127aad2dfb508b8
Rohan Rane <rohanrane@Rohans-MacBook-Air.local>
2025-06-06 23:25:41 -0700

    port fix

--- server/app.py
+++ server/app.py
@@ -142,5 +142,4 @@ def get_shortest_path():
     return jsonify({"path": []})
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(debug=True, host="0.0.0.0", port=port)
+    app.run(debug=True)
