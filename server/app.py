3b82f5a243b0cc430547ad6d74d543d3be0500e4
ryrane1 <ryrane@gmail.com>
2025-06-12 08:26:48 -0700

    fix app.py

--- server/app.py
+++ server/app.py
@@ -291,5 +291,5 @@ def get_daily_leaderboard():
     return jsonify(ref.get() or [])
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5001)
-    app.run(debug=True, host="0.0.0.0", port=port)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
